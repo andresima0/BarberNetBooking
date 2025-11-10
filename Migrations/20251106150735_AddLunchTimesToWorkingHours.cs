5f3fc91016b3f4dd5bfccb0c5a034461543cdfbc
andresima0 <andresimaodesouza@gmail.com>
2025-11-10 10:58:07 -0300

    feat: added unversioned files

---
+++ Migrations/20251106150735_AddLunchTimesToWorkingHours.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberNetBooking.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLunchTimesToWorkingHours : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "LunchEndTime",
+                table: "WorkingHours",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "LunchStartTime",
+                table: "WorkingHours",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LunchEndTime",
+                table: "WorkingHours");
+
+            migrationBuilder.DropColumn(
+                name: "LunchStartTime",
+                table: "WorkingHours");
+        }
+    }
+}
---
+++ Migrations/20251108200318_AddBrandingFields.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberNetBooking.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBrandingFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogoPath",
+                table: "ShopInfos",
+                type: "TEXT",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SiteName",
+                table: "ShopInfos",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoPath",
+                table: "ShopInfos");
+
+            migrationBuilder.DropColumn(
+                name: "SiteName",
+                table: "ShopInfos");
+        }
+    }
+}
